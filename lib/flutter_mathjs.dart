cfca0ebcdcd9e47b6bdff1193c6da52fc8e6ccea
Ujang Karnadi <uk9313285@gmail.com>
2019-08-10 05:45:36 +0700

    update description

--- lib/flutter_mathjs.dart
+++ lib/flutter_mathjs.dart
@@ -6,7 +6,15 @@ class Mathjs {
   static const MethodChannel _channel =
       const MethodChannel('flutter_mathjs');
 
+
+ /// evaluate the given expression
   static Future<String> eval(String exp) async {
+    final String result = await _channel.invokeMethod('eval',{'exp':exp});
+    return result;
+  }
+
+  /// Clear all variable created by eval method
+  static Future<String> clear(String exp) async {
     final String result = await _channel.invokeMethod('eval',{'exp':exp});
     return result;
   }
--- test/flutter_mathjs_test.dart
+++ test/flutter_mathjs_test.dart
@@ -16,6 +16,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await FlutterMathjs.platformVersion, '42');
+    // expect(await Mathjs, '42');
   });
 }
